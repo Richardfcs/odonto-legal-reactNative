5159da89305fb0e6080fe03d090288692316e4d3
Richardfcs <166066749+Richardfcs@users.noreply.github.com>
2025-05-28 15:53:55 +0000

    1.0.3

--- odontoforense/App.js
+++ odontoforense/App.js
@@ -1,6 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from './screens/login'
+
 // import AdminHomeScreen from './screensAdm/home'
 import MainHomeScreen from './screens/home'
 
@@ -11,6 +12,7 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
         <Stack.Screen name="Login" component={LoginScreen} />
+        
         {/* <Stack.Screen name="AdminHome" component={AdminHomeScreen} /> */}
         <Stack.Screen name="MainHome" component={MainHomeScreen} />
       </Stack.Navigator>
